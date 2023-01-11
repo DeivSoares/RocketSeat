a8f5aa4eefd040f290ac474a76f76338d51d87a0
Deivison Soares <deivisonsoares1610@outlook.com>
2023-01-11 05:48:30 -0300

    .

---
+++ Discover/js/scripts.js
@@ -0,0 +1,28 @@
+let score = 80;
+function getScore() {
+  let scoreA = score >= 90 && score <= 100;
+  let scoreB = score >= 80 && score <= 89;
+  let scoreC = score >= 70 && score <= 79;
+  let scoreD = score >= 60 && score <= 69;
+  let scoreF = score < 60 && score >= 0;
+
+  let scoreFinal;
+
+  if (scoreA) {
+    scoreFinal = "A";
+  } else if (scoreB) {
+    scoreFinal = "B";
+  } else if (scoreC) {
+    scoreFinal = "C";
+  } else if (scoreD) {
+    scoreFinal = "D";
+  } else if (scoreF) {
+    scoreFinal = "F";
+  } else {
+    scoreFinal = "Nota Inválida";
+  }
+
+  return scoreFinal
+}
+
+getScore(101)
